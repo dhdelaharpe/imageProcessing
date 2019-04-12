9cbb5a8adfa3151493c75af0bf26bafc2062e71c
dhdelaharpe <36035708+dhdelaharpe@users.noreply.github.com>
2019-04-13 01:39:00 +0200

    Add files via upload

--- stuff/mark.py
+++ stuff/mark.py
@@ -6,6 +6,7 @@ import imutils
 from scipy.spatial import distance as dist
 from operator import itemgetter
 def order_points(pts):
+	'''function to arrange co ords of contour from ##this approach can be found at: https://www.pyimagesearch.com/2016/10/03/bubble-sheet-multiple-choice-scanner-and-test-grader-using-omr-python-and-opencv/'''
 	# sort the points based on their x-coordinates
 	xSorted = pts[np.argsort(pts[:, 0]), :]
  
@@ -33,6 +34,7 @@ def order_points(pts):
 	return np.array([tl, tr, br, bl], dtype="float32")
 
 def four_point_transform(image, pts):
+	'''function to transform image according to 4 points from ##this approach can be found at: https://www.pyimagesearch.com/2016/10/03/bubble-sheet-multiple-choice-scanner-and-test-grader-using-omr-python-and-opencv/'''
 	# obtain a consistent order of the points and unpack them
 	# individually
 	rect = order_points(pts)
@@ -69,22 +71,59 @@ def four_point_transform(image, pts):
  
 	# return the warped image
 	return warped
-#pages= convert_from_path("MCQ_600dpi_2016.pdf")
+
+#pages= convert_from_path("2018.pdf")
 #for i in range(len(pages)):
-#	pages[i].save('out'+str(i)+'.png','PNG')
-img = cv2.imread("out7.png")
+#	pages[i].save('out2018'+str(i)+'.png','PNG')
+img = cv2.imread("out20181.png")
 img = cv2.resize(img,(210*3,297*3))
+
+######################### First Orientate the image with sift, flann matcher, homography and warp####################
+MIN_MATCH_COUNT = 10
+imgTemplate = cv2.imread('out0.png') # TEMPLATE TO HELP ORIENTATION
+imgTemplate = cv2.resize(imgTemplate,(210*3,297*3))#size it to the same as current image
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img,None)
+kp2, des2 = sift.detectAndCompute(imgTemplate,None)
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1,des2,k=2)
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    im_dst = np.zeros(img.shape, np.uint8)
+    img = cv2.warpPerspective(img, M, (im_dst.shape[1],im_dst.shape[0])) #WARP to match template
+
+
+
+
+#############################################################
+#now we can begin 
+#############################################################
+#FILTERS
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ANSWERS ={1:0, 2:1,3:2}
-kernel = np.ones((5,5),np.uint8)
-gBlur = cv2.GaussianBlur(gray, (5, 5), 0)
-#normImg = cv2.normalize(gBlur, None, 0, 255, cv2.NORM_MINMAX)
-edged = cv2.Canny(gBlur,100,200)
+ANSWERS ={1:0, 2:1,3:2} 
+kernel = np.ones((3,3),np.uint8)	#kernel specification to be used with morphing
+gBlur = cv2.GaussianBlur(gray, (5, 5), 0)	#typical gblur to help processing
+#normImg = cv2.normalize(gBlur, None, 0, 255, cv2.NORM_MINMAX)		#normalizing - still to test how this helps
+edged = cv2.Canny(gBlur,100,200)		#canny edge detection prior to contouring
 
-closing = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+closing = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)	#closing to help clear the lines
+##############################################################
 
-# find contours in the edge map, then initialize
-# the contour that corresponds to the document
+##############################################################
+#FIND EDGES AND TRANSFORM
 cnts = cv2.findContours(closing, cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
@@ -107,123 +146,242 @@ if len(cnts) > 0:
 		if len(approx) == 4:
 			docCnt=approx
 			break
-
-
+#now we know the corners - this picks up the small boxes at each corner
+#need to transform the image to only include what is inside those corners
 # apply a four point perspective transform to both the
 # original image and grayscale image to obtain a top-down
 # birds eye view of the paper
 paper = four_point_transform(img, docCnt.reshape(4, 2))
 warped = four_point_transform(gray, docCnt.reshape(4, 2))
 bWarped = cv2.GaussianBlur(warped, (3, 3), 0)
-#warped = cv2.morphologyEx(warped, cv2.MORPH_CLOSE, kernel)
+#############################################################
 
-# apply Otsu's thresholding method to binarize the warped
-# piece of paper
-thresh = cv2.threshold(bWarped, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-width,height = bWarped.shape[:2]
-#split into sections
-studentSection= bWarped[110:height*2,25:np.int0(width/4)]
-sw,sh = studentSection.shape[:2]
-#get task
-taskSection = studentSection[sh+80:sh+300,100:150].copy()
-#remove task from studentSection
-studentSection[sh+60:sh+300,100:150]=255
+#############################################################
+#SPLIT IMAGE INTO PIECES
+height,width = bWarped.shape[:2]
 
-answersSection1= bWarped[0:height*2,np.int0(width/4):np.int0(width/2)-25]
-answersSection2= bWarped[0:height*2,np.int0(width/2):width-250]
-#split answers into blocks
-#width,height=answersSection1.shape[:2]
-h = np.int0(height/4)
-answerBlock1 = answersSection1[0:h,0:width]
-answerBlock2 = answersSection1[h:np.int0(h*1.8),0:width]
-answerBlock3 = answersSection1[np.int0(h*1.8):np.int0(h*2.6),0:width]
-answerBlock4 = answersSection1[np.int0(h*2.6):np.int0(h*3.4),0:width]
-answerBlock5 = answersSection1[np.int0(h*3.4):np.int0(h*4.2),0:width]
-answerBlock6 = answersSection1[np.int0(h*4.2):np.int0(h*5.05),0:width]
 
-answerBlock7 = answersSection2[0:h,0:width]
-answerBlock8 = answersSection2[h:np.int0(h*1.8),0:width]
-answerBlock9 = answersSection2[np.int0(h*1.8):np.int0(h*2.6),0:width]
-answerBlock10 = answersSection2[np.int0(h*2.6):np.int0(h*3.4),0:width]
-answerBlock11 = answersSection2[np.int0(h*3.4):np.int0(h*4.2),0:width]
-answerBlock12 = answersSection2[np.int0(h*4.2):np.int0(h*5.05),0:width]
+studentSection= bWarped[np.int0(height*0.15):np.int0(height*0.85),np.int0(width*0.05):np.int0(width*0.31)]
+sh,sw = studentSection.shape[:2]
+
+taskSection = studentSection[np.int0(sh*0.46):np.int0(sh*0.85),np.int0(sw*0.66):np.int0(sw*0.95)].copy()
+
+studentSection[np.int0(sh*0.42):np.int0(sh*0.85),np.int0(sw*0.66):np.int0(sw*0.95)]=255
+
+answersSection1= bWarped[np.int0(height*0.045):np.int0(height*0.95),np.int0(width*0.43):np.int0(width*0.6)]
+answersSection2= bWarped[np.int0(height*0.045):np.int0(height*0.95),np.int0(width*0.72):np.int0(width*0.90)]
+
+h,w = answersSection1.shape[:2]
+answerBlock1 = answersSection1[0:np.int0(h*0.15),0:width]
+answerBlock2 = answersSection1[np.int0(h*0.17):np.int0(h*0.31),0:width]
+answerBlock3 = answersSection1[np.int0(h*0.34):np.int0(h*0.48),0:width]
+answerBlock4 = answersSection1[np.int0(h*0.51):np.int0(h*0.65),0:width]
+answerBlock5 = answersSection1[np.int0(h*0.68):np.int0(h*0.82),0:width]
+answerBlock6 = answersSection1[np.int0(h*0.85):np.int0(h*0.98),0:width]
+
+h,w = answersSection2.shape[:2]
+answerBlock7 = answersSection2[0:np.int0(h*0.15),0:width]
+answerBlock8 = answersSection2[np.int0(h*0.17):np.int0(h*0.31),0:width]
+answerBlock9 = answersSection2[np.int0(h*0.34):np.int0(h*0.48),0:width]
+answerBlock10 = answersSection2[np.int0(h*0.52):np.int0(h*0.65),0:width]
+answerBlock11 = answersSection2[np.int0(h*0.68):np.int0(h*0.82),0:width]
+answerBlock12 = answersSection2[np.int0(h*0.85):np.int0(h*0.98),0:width]
+
 #put them all in an array
 answerBlocks=np.array([answerBlock1,answerBlock2,answerBlock3,answerBlock4,answerBlock5,answerBlock6,answerBlock7,answerBlock8,answerBlock9,answerBlock10,answerBlock11,answerBlock12])
+#############################################################
 
 def findCircles(section):	
-	'''functions to find circles'''
+	'''functions to find circles using hough circles'''
 	questionCircles=[]
-	circles = cv2.HoughCircles(section,cv2.HOUGH_GRADIENT,1,15,
+	circles = cv2.HoughCircles(section,cv2.HOUGH_GRADIENT,1,14,
 								param1=10,param2=10,minRadius=7,maxRadius=9)
 	circles = np.uint16(np.around(circles))
 	questionCircles=circles.copy()
-
-	for i in circles[0,:]:
+	#for i in circles[0,:]:
 		# draw the outer circle
-		cv2.circle(section,(i[0],i[1]),i[2],(0,255,0),2)
+		#cv2.circle(section,(i[0],i[1]),i[2],(0,255,0),3)
 		# draw the center of the circle
 		#cv2.circle(studentSection,(i[0],i[1]),2,(0,0,255),3)
+	cv2.imshow("f",section)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
 	return questionCircles
 
+kernel = np.ones((3,3),np.uint8)	#kernel specification to be used with morphing
 
-def getTaskNo(circles):
-	circles= circles[0,:]
-	circles = sorted(circles,key=itemgetter(1))
-	row = 0
-	score =[]
-	thresh=cv2.threshold(block, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
+def approximiteMissingCircle(circles,expectedAmt):
+	'''function to approximate missing circles'''
+	count=0
+	i=0
+	while(i<24):	#<24 since 25 circles expected, could adjust this to passed value to apply to task section ?
+		if(count!=4):#when count is 4, we have a group of 5 and expect a jump in values -- no need to check those
+			one = int(circles[i][1])
+			if(i==len(circles)-1):#at this point we don't have a group of 5 and don't have any more to check, force program to go into approximation
+				two=0
+			else:
+				two=int(circles[i+1][1])
+			num = abs(one-two)	
+			if(num>5 or two==0) :#difference between y values is large, then it should be in a different row and we know where we are missing one
+				temp = circles[i-count:i+1]	#get a group of circles for the row
+				temp = sorted(temp,key=itemgetter(0))#sort them
+				for y in range(count):	
+					x=int(temp[y+1][0])-int(temp[y][0]) #check x difference between adjacent circle
+					if(x>20):	#large difference, we know where the gap  is
+						circles=np.insert(circles,i,np.array([np.uint16(16+temp[y][0]),temp[y][1],temp[y][2]],dtype=np.uint16),0)#approximate circle location
+						i-=1	#force loop to recheck this area in case another missing circle in row
+						count-=1
+					elif(y==count-1):
+						#we've not found any gaps, gap must be at start or end of group
+						if(int(temp[0][0])-10>8):#10 is expected start value
+							#gap is at first element
+							temp = np.array([np.uint16(temp[0][0]-16),temp[0][1],temp[0][2]])
+							circles=np.insert(circles,i-count,temp,0)
+						elif(78-int(temp[count-1][0])>8):#78 is expected end value
+							#gap is at last element
+							temp = np.array([np.uint16(temp[count][0]+16),temp[count][1],temp[count][2]])
+							circles=np.insert(circles,i+1,temp,0)
+						count-=1
+						i-=1
+				if(len(circles)==expectedAmt):break
+		count=0 if count==4 else count+1
+		i+=1
+	return circles
 def scoreQuestion(circles,block,perRow=5):
-	circles=circles[0,:]
-	#sort circles by row
-	circles=sorted(circles,key=itemgetter(1))
-	correct=0 #store correct answers
-	questionNumber=0
-	score = []
-	thresh=cv2.threshold(block, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-	#then sort by column
-	for q,i in enumerate(np.arange(0,len(circles),perRow)):
-		cnts = sorted(circles[i:i+perRow],key=itemgetter(0))
-		questionNumber+=1
-		answer=[]#-1 for no answer, -2 for disqualified (answered too many or badly for this question)
+	'''function to score whether a circle has been marked'''
+	circles=circles[0,:]	#only values we care about
+	circles=sorted(circles,key=itemgetter(1))#sort circles by row
+	magnitude=5 if perRow==5 else 10	
+
+	if(len(circles)!=perRow*magnitude):#we're missing a circle possible obfuscation, need to find the whole and fill it
+		circles=approximiteMissingCircle(circles,perRow*magnitude)
 		
-		#now start checking for correct
-			# loop over the sorted contours
-		for (j, c) in enumerate(cnts):
+	score = []	#array to track results
+	thresh=cv2.threshold(block,170,255,cv2.THRESH_BINARY_INV)[1]#binary inverse to prepare image for counting
+
+	for _,i in enumerate(np.arange(0,len(circles),perRow)):#loop through by row
+		circ = sorted(circles[i:i+perRow],key=itemgetter(0))	#sort row by x
+		answer=[]#-1 for no answer, -2 for disqualified (answered too many or badly for this question)
+	
+		for (j, c) in enumerate(circ):#loop over row
 			#points of current circle
+			area=[]
 			x = c[0]
 			y = c[1]
-			r=c[2]
-			print(j)
-			closed = cv2.erode(thresh,kernel,1)
-			area= closed[y-r:y+r,x-r:x+r]
-			count = cv2.countNonZero(area)
-			#cv2.imshow("q",area)
-			#cv2.imshow("block",thresh)
-			#cv2.waitKey(0)
-			if(count>5):
+			r=c[2]-3
+			area= thresh[y-r:y+r,x-r:x+r]	#area of interest 
+			count = cv2.countNonZero(area)	#check non zero values in roi
+			
+			if(count>40):	#found enough to say its a marked circle
 				answer.append(j)
-		if(len(answer)==0):
+		if(len(answer)==0):	#didn't find any marked circles
 			answer.append(-1)
 		score.append(answer)
 	return score
 
+def scoreStudentNo(circles,block):
+	'''function to score whether a circle has been marked for the student section--to account for the different nature of the box'''
+	circles=circles[0,:]
+	#sort circles by row
+	circles=sorted(circles,key=itemgetter(0))
+
+	thresh=cv2.threshold(block,170,255,cv2.THRESH_BINARY_INV)[1]
+
+	marks=[]
+	chr1Arr = sorted(circles[0:10],key=itemgetter(1))
+	chr2Arr = sorted(circles[10:20],key=itemgetter(1))
+	chr3Arr = sorted(circles[20:46],key=itemgetter(1))
+	chr4Arr = sorted(circles[46:56],key=itemgetter(1))
+	chr5Arr = sorted(circles[56:66],key=itemgetter(1))
+	chr6Arr = sorted(circles[66:76],key=itemgetter(1))
+	chr7Arr = sorted(circles[76:86],key=itemgetter(1))
+	for j,c in enumerate(chr3Arr):
+		mark=-1
+		x=c[0]
+		y=c[1]
+		r=c[2]-3
+
+		area= thresh[y-r:y+r,x-r:x+r]
+		count = cv2.countNonZero(area)
+
+		if(count>40):
+			mark=j
+			break
+	marks.append(mark)
+	nums=[chr1Arr,chr2Arr,chr4Arr,chr5Arr,chr6Arr,chr7Arr]
+	for chrArr in nums:
+			for j,c in enumerate(chrArr):
+				mark=-1
+				x=c[0]
+				y=c[1]
+				r=c[2]-3
+				#closed = cv2.morph(thresh,kernel,1)
+				area= thresh[y-r:y+r,x-r:x+r]
+				count = cv2.countNonZero(area)
+				if(count>40):
+					mark=j
+					break
+			marks.append(mark)
+	return marks
+
+
+def getLetter(chr):
+	'''function to return a letter from a index'''
+	alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	if(len(chr)>1):
+		out=""
+		for x in chr:
+			out+=alphabet[x]
+		return out
+	return "No Selection" if chr[0]==-1 else alphabet[chr[0]]
+
+def getTaskNo(taskArr):
+	'''function to return task number from array of marked circles'''
+	out1=""
+	out2=""
+	for i in range(len(taskArr)):
+		if(len(taskArr[i])>1):
+			out1=str(i)
+			out2=str(i)
+			break
+		if(taskArr[i][0]>=0):
+			if(taskArr[i][0]==0):
+				if(out1!=""): return "Invalid Task Number"
+				out1=str(i)
+			else:
+				if(out2!=""): return "Invalid Task Number"
+				out2=str(i)
+	
+	return "Invalid Task Number" if out1+out2=="" else out1+out2
+
+################PROCESS TASK SECTION###############
 taskCircles = findCircles(taskSection)
 taskNo=scoreQuestion(taskCircles, taskSection,2)
+taskNo=getTaskNo(taskNo)
+##############PROCESS STUDENT SECTION##############
 studentCircles= findCircles(studentSection)
-studentNo = scoreQuestion(studentCircles,studentSection,7)
-cv2.imshow("stu",studentSection)
-cv2.waitKey(0)
-print(studentNo)
+stuArr= scoreStudentNo(studentCircles,studentSection)
+studentNo = str(stuArr[1])+str(stuArr[2])+getLetter([stuArr[0]])+str(stuArr[3])+str(stuArr[4])+str(stuArr[5])+str(stuArr[6])
+##############PROCESS ANSWER SECTION###############
+score = []
+
 for block in answerBlocks:
 	circles=findCircles(block)
-	score = scoreQuestion(circles,block)
-	print (score)
-	cv2.imshow("answer1",block)
-	cv2.waitKey(0)
-#cv2.imshow("hough",studentSection)
+	score.append(scoreQuestion(circles,block))
 
-#cv2.imshow("answer1",answersSection1)
-#cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+################PROCESS RESULTS###################
+
+f = open("output.csv","w")
+data="student_number, task, question, answers\n"
+answers=""
+questionNo=1
+
+for line in score:
+	for l in line:
+		data+=studentNo+", "+taskNo+", "+str(questionNo)+", "+getLetter(l)+"\n"
+		questionNo+=1
+
+f.write(data)
+f.close()
